0981a421c1e077d8469582385d043fcc00b05322
Thinh Nguyen <thinh.nguyen@emc.com>
2016-05-12 13:49:46 -0400

    Put skip key word for CI in the same line
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- ci/tasks/promote-scaleio-sdc-candidate.sh
+++ ci/tasks/promote-scaleio-sdc-candidate.sh
@@ -15,32 +15,31 @@ echo ${integer_version} > promote/integer_version
 
 cp -r scaleio-sdc-bosh-release promote/scaleio-sdc-bosh-release
 pushd promote/scaleio-sdc-bosh-release
+  set +x
+  echo creating config/private.yml with blobstore secrets
+  cat > config/private.yml << EOF
+  ---
+  blobstore:
+    s3:
+      bucket_name: scaleio-sdc-bosh-release
+      access_key_id: ${S3_ACCESS_KEY_ID}
+      secret_access_key: ${S3_SECRET_ACCESS_KEY}
+  EOF
+  set -x
 
-set +x
-echo creating config/private.yml with blobstore secrets
-cat > config/private.yml << EOF
----
-blobstore:
-  s3:
-    bucket_name: scaleio-sdc-bosh-release
-    access_key_id: ${S3_ACCESS_KEY_ID}
-    secret_access_key: ${S3_SECRET_ACCESS_KEY}
-EOF
-set -x
+  echo "using bosh CLI version..."
+  bosh version
 
-echo "using bosh CLI version..."
-bosh version
+  echo "finalizing scaleio sdc release..."
+  echo '' | bosh create release --force --with-tarball --version ${integer_version} --name ${SCALEIO_SDC_RELEASE_NAME}
+  bosh finalize release dev_releases/${SCALEIO_SDC_RELEASE_NAME}/*.tgz --version ${integer_version}
 
-echo "finalizing scaleio sdc release..."
-echo '' | bosh create release --force --with-tarball --version ${integer_version} --name ${SCALEIO_SDC_RELEASE_NAME}
-bosh finalize release dev_releases/${SCALEIO_SDC_RELEASE_NAME}/*.tgz --version ${integer_version}
+  rm config/private.yml
 
-rm config/private.yml
+  git diff | cat
+  git add .
 
-git diff | cat
-git add .
-
-git config --global user.email emccmd-eng@emc.com
-git config --global user.name EMCCMD-CI
-git commit -m ":airplane: New final release v ${integer_version}" -m "[ci skip]"
+  git config --global user.email emccmd-eng@emc.com
+  git config --global user.name EMCCMD-CI
+  git commit -m ":airplane: New final release v ${integer_version} [ci skip]"
 popd
