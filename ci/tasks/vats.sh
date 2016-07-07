a570596e7c4ffd0d4b829b929c4f3abd38218b33
Gary Preston White <Gary.WhiteJr@emc.com>
2016-07-07 16:18:14 -0400

    Adapt pipeline to vsphere
    - Use go get -insecure for dependencies
    
    Signed-off-by: Thinh Nguyen <thinh.nguyen@emc.com>

--- ci/tasks/vats.sh
+++ ci/tasks/vats.sh
@@ -3,9 +3,6 @@
 set -e -x
 source rexray-bosh-release/ci/tasks/utils.sh
 
-check_param AWS_ELASTIC_IP
-check_param AWS_SECURITY_GROUP
-check_param AWS_SUBNET_ID
 check_param BOSH_DIRECTOR_PUBLIC_IP
 check_param BOSH_PASSWORD
 check_param BOSH_USER
@@ -15,6 +12,7 @@ check_param DEPLOYMENT_PRIVATE_KEY
 check_param FAKE_VOLUME_NAME
 check_param REXRAY_RELEASE_NAME
 check_param SCALEIO_ENDPOINT
+check_param SCALEIO_INSECURE
 check_param SCALEIO_MDM_IPS
 check_param SCALEIO_PASSWORD
 check_param SCALEIO_PROTECTION_DOMAIN_ID
@@ -26,72 +24,42 @@ check_param SCALEIO_SYSTEM_ID
 check_param SCALEIO_USER_ID
 check_param SCALEIO_USERNAME
 check_param SCALEIO_VERSION
+check_param VSPHERE_VM_IP
 
 bosh target ${BOSH_DIRECTOR_PUBLIC_IP}
 bosh login ${BOSH_USER} ${BOSH_PASSWORD}
-bosh upload stemcell stemcell/stemcell.tgz || true
 
 cat > scaleio-acceptance-manifest.yml <<EOF
 ---
-name: ${DEPLOYMENT_NAME}
-director_uuid: $(bosh status --uuid)
-
+name: scaleio_rexray
+director_uuid: cd0eb8bc-831e-447d-99c1-9658c76e7721
+stemcells:
+- alias: trusty
+  os: ubuntu-trusty
+  version: '3215'
 releases:
-- name: ${SCALEIO_SDC_RELEASE_NAME}
+- name: rexray-bosh-release
   version: latest
-- name: ${REXRAY_RELEASE_NAME}
+- name: scaleio-sdc-bosh-release
   version: latest
-
-disk_pools:
-- name: disks
-  disk_size: 20_000
-  cloud_properties: {type: gp2}
-
-resource_pools:
-- name: vms
-  network: private
-  stemcell:
-    name: bosh-aws-xen-hvm-ubuntu-trusty-go_agent
-    version: 3215
-  cloud_properties:
-    instance_type: m3.medium
-    ephemeral_disk: {size: 25_000, type: gp2}
-    availability_zone: us-east-1a
-
-networks:
-- name: private
-  type: manual
-  subnets:
-  - range: 10.0.0.0/16
-    gateway: 10.0.0.1
-    dns: [10.0.0.1]
-    reserved:
-    - 10.0.0.2 - 10.0.0.18
-    static:
-    - 10.0.0.20
-    cloud_properties:
-      subnet: ${AWS_SUBNET_ID}
-      security_groups:
-        - ${AWS_SECURITY_GROUP}
-- name: public
-  type: vip
-
 jobs:
-- name: scaleio-sdc-with-rexray
+- name: scaleio_rexray
   instances: 1
   templates:
-  - {name: setup_sdc, release: ${SCALEIO_SDC_RELEASE_NAME}}
-  - {name: rexray_service , release: ${REXRAY_RELEASE_NAME}}
-  resource_pool: vms
+  - name: setup_sdc
+    release: scaleio-sdc-bosh-release
+  - name: rexray_service
+    release: rexray-bosh-release
+  vm_type: medium
+  stemcell: trusty
+  azs:
+  - z1
   networks:
   - name: private
-    static_ips: [10.0.0.20]
-    default: [dns, gateway]
-  - name: public
-    static_ips: [${AWS_ELASTIC_IP}]
+    static_ips:
+    - ${VSPHERE_VM_IP}
   properties:
     network_name: private
-
 properties:
   scaleio:
     mdm:
@@ -109,9 +77,6 @@ properties:
           desc: The default docker module.
           disabled: false
           host: tcp://127.0.0.1:9000
-          http:
-            writetimeout: 120
-            readtimeout: 120
           spec: /etc/docker/plugins/rexray.spec
           type: docker
       libstorage:
@@ -120,38 +85,30 @@ properties:
         service: scaleio
     scaleio:
       endpoint: ${SCALEIO_ENDPOINT}
-      insecure: true
+      insecure: ${SCALEIO_INSECURE}
       password: ${SCALEIO_PASSWORD}
-      protectionDomainID: ${SCALEIO_PROTECTION_DOMAIN_ID}
-      protectionDomainName: ${SCALEIO_PROTECTION_DOMAIN_NAME}
-      storagepoolID: ${SCALEIO_STORAGE_POOL_ID}
+      protectionDomainName: default
       storagePoolName: ${SCALEIO_STORAGE_POOL_NAME}
       systemID: ${SCALEIO_SYSTEM_ID}
       thinOrThick: ThinProvisioned
-      useCerts: true
+      useCerts: false
       userID: ${SCALEIO_USER_ID}
       userName: ${SCALEIO_USERNAME}
       version: ${SCALEIO_VERSION}
     linux:
       volume:
         fileMode: 0777
-compilation:
-  workers: 1
-  network: private
-  reuse_compilation_vms: true
-  cloud_properties:
-    name: random
-    instance_type: m3.medium
-    availability_zone: us-east-1a
-
 update:
   canaries: 1
   max_in_flight: 3
   canary_watch_time: 30000-600000
   update_watch_time: 5000-600000
+
 EOF
 cat scaleio-acceptance-manifest.yml
 bosh deployment scaleio-acceptance-manifest.yml
+
+apt-get -y update && apt-get -y install sshpass
 
 pushd scaleio-sdc-bosh-release
   bosh -n create release --force --name ${SCALEIO_SDC_RELEASE_NAME}
@@ -164,18 +121,18 @@ pushd rexray-bosh-release
 popd
 
 function cleanUp {
+  local status=$?
   bosh -n delete deployment ${DEPLOYMENT_NAME}
   bosh -n delete release ${REXRAY_RELEASE_NAME}
   bosh -n delete release ${SCALEIO_SDC_RELEASE_NAME}
+  exit $status
 }
 trap cleanUp EXIT
 
 bosh -n deploy
 
-echo "${DEPLOYMENT_PRIVATE_KEY}" > bosh.pem
-chmod 600 bosh.pem
 function ssh_run() {
-  ssh -o "StrictHostKeyChecking no" -i bosh.pem vcap@${AWS_ELASTIC_IP} \
+  sshpass -p ${DEPLOYMENT_PASSWORD} ssh -o "StrictHostKeyChecking no" vcap@${VSPHERE_VM_IP} \
     "echo ${DEPLOYMENT_PASSWORD} | sudo -S bash -c ' $* '"
 }
 
@@ -189,13 +146,16 @@ cat > config.json <<EOF
   }
 }
 EOF
-scp -o StrictHostKeyChecking=no -i bosh.pem config.json vcap@${AWS_ELASTIC_IP}:/home/vcap/
+
+sshpass -p ${DEPLOYMENT_PASSWORD} scp -o StrictHostKeyChecking=no config.json vcap@${VSPHERE_VM_IP}:/home/vcap/
 
 cat > run_test.sh <<EOF
 set -x
-wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
+wget --no-check-certificate https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 tar -zxf go1.6.2.linux-amd64.tar.gz -C ./
 export GOROOT=/home/vcap/go
+export GOOS=linux
+export GOARCH=amd64
 export PATH=\$PATH:\$GOROOT/bin
 
 mkdir -p gocode
@@ -204,22 +164,35 @@ export PATH=\$PATH:\$GOPATH/bin
 
 /var/vcap/packages/rexray/rexray volume create --volumename ${FAKE_VOLUME_NAME} --size 8 || true
 
-apt-get -y update && apt-get -y install git
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega
-go get github.com/cloudfoundry-incubator/volume_driver_cert
-go install github.com/onsi/ginkgo/ginkgo
+apt-get -y update && apt-get -y install git && apt-get -y install jq
 
+mkdir -p \$GOPATH/src/gopkg.in/
+cd \$GOPATH/src/gopkg.in/
+git clone https://github.com/go-yaml/yaml.git yaml.v2
+go install gopkg.in/yaml.v2
+
+mkdir -p \$GOPATH/src/github.com/onsi
+cd \$GOPATH/src/github.com/onsi
+
+git clone https://github.com/onsi/ginkgo.git
+git clone https://github.com/onsi/gomega.git
+go install github.com/onsi/ginkgo/ginkgo
+go install github.com/onsi/gomega
+
+go get --insecure -f -u github.com/cloudfoundry-incubator/volume_driver_cert
+go get --insecure -f -u code.cloudfoundry.org/clock
 cd \$GOPATH/src/github.com/cloudfoundry-incubator/volume_driver_cert
-go get -t ./...
 
 printf "http://127.0.0.1:9000" > /etc/docker/plugins/rexray.spec
 
 export FIXTURE_FILENAME=/home/vcap/config.json
-ginkgo
+ginkgo -r
 
 /var/vcap/packages/rexray/rexray volume unmount --volumename ${FAKE_VOLUME_NAME} || true
+/var/vcap/packages/rexray/rexray volume remove --volumeid $(/var/vcap/packages/rexray/rexray volume get --volumename ${FAKE_VOLUME_NAME} -f json | jq .id -r)
 EOF
+
 chmod +x run_test.sh
-scp -o StrictHostKeyChecking=no -i bosh.pem run_test.sh vcap@${AWS_ELASTIC_IP}:/home/vcap/
+sshpass -p ${DEPLOYMENT_PASSWORD} scp -o StrictHostKeyChecking=no run_test.sh vcap@${VSPHERE_VM_IP}:/home/vcap/
+
 ssh_run ./run_test.sh
