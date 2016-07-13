b664edeaa1db2cb51a563d2f16e7ef97f0b3d5b4
Peter Blum <Peter.Blum@emc.com>
2016-07-13 10:02:06 -0400

    Volman now using go get instead of vendoring

--- ci/tasks/vats-isilon.sh
+++ ci/tasks/vats-isilon.sh
@@ -148,6 +148,10 @@ go get --insecure -f -u github.com/onsi/ginkgo/ginkgo
 go get --insecure -f -u github.com/onsi/gomega
 go get --insecure -f -u github.com/cloudfoundry-incubator/volume_driver_cert
 go get --insecure -f -u code.cloudfoundry.org/clock
+go get --insecure -f -u code.cloudfoundry.org/cfhttp
+go get --insecure -f -u code.cloudfoundry.org/cfhttp/handlers
+go get --insecure -f -u github.com/cloudfoundry/gunk/http_wrap
+
 cd \$GOPATH/src/github.com/cloudfoundry-incubator/volume_driver_cert
 
 export FIXTURE_FILENAME=/home/vcap/config.json
